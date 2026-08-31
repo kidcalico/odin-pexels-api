975fc864df6bd47758b05b67a6ecbe9eb13113ed
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-08-31 14:18:43 -0700

    Fix Controller and view
    Display "Collection not found" if the collection cannot be accessed via
    the API.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
     connection = PexelsApiClient.new
-    @collection = connection.collection(params[:id])
-    @photos = @collection["media"].map do |photo|
-      photo["src"]["medium"]
+    if params[:id]
+      @collection = connection.collection(params[:id])
+      if @collection.success?
+        @photos = @collection["media"].map do |photo|
+          photo["src"]["medium"]
+        end
+      else
+        @photos = []
+      end
     end
   end
 end
